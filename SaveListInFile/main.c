6be289152e4634f0575705a0e3f519e558356e59
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-07-16 22:46:29 -0300

    Testes da funçao removeNode

--- SaveListInFile/main.c
+++ SaveListInFile/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "util.h"
-#include "linked_list.h"
+#include "linked_list.c"
 
 int main(const int argc, const char** argv) {
 
@@ -21,7 +21,21 @@ int main(const int argc, const char** argv) {
   puts("-----");
   printList(list);
 
-  pop(list);
+  printf("\n\n\n%d --> %d\n", list->head->id, list->head->next->id);
+  removeNode(list, 0);
+  printf("%d --> %d\n", list->head->id, list->head->next->id);
+  removeNode(list, 1);
+  printf("%d --> %d\n", list->head->id, list->head->next->id);
+  removeNode(list, 2);
+  printf("%d --> %d\n", list->head->id, list->head->next->id);
+  removeNode(list, 3);
+  printf("%d --> %d\n", list->head->id, list->head->next->id);
+  removeNode(list, 4);
+  printf("%d -->\n", list->head->id);
+
+  printf(" >>> %d\n", list->head->id);
+  removeNode(list, 5);
+
 
   puts("-----");
   printList(list);
