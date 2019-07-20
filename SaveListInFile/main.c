e8e33147fdae16f6ca1c0bdefb8589e6aea195bd
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-07-20 00:03:10 -0300

    Hmm

--- SaveListInFile/main.c
+++ SaveListInFile/main.c
@@ -11,38 +11,19 @@ int main() {
     return EXIT_FAILURE;
   }  
 
-  addNode(list, "Olá mundo!");
-  addNode(list, "AAA doidera!");
-  addNode(list, "Teste!");
-  addNode(list, "Teste2!");
-  addNode(list, "Teste3!");
-  addNode(list, "Teste4!");
-  
-  puts("-----");
-  printList(list);
+  addNode(list, "olá mundo! 0");
+  addNode(list, "olá mundo! 1");
+  addNode(list, "olá mundo! 2");
+  addNode(list, "olá mundo! 3");
 
-  printf("\n\n\n%d --> %d\n", list->head->id, list->head->next->id);
-  removeNode(list, 5);
-  printf("%d --> %d\n", list->head->id, list->head->next->id);
-  removeNode(list, 4);
-  printf("%d --> %d\n", list->head->id, list->head->next->id);
-  removeNode(list, 3);
-  printf("%d --> %d\n", list->head->id, list->head->next->id);
-  removeNode(list, 2);
-  printf("%d --> %d\n", list->head->id, list->head->next->id);
   removeNode(list, 1);
-  printf("%d -->\n", list->head->id);
 
-  printf(" >>> %d\n", list->head->id);
-  removeNode(list, 0);
+  addNode(list, "olá mundo! 4");
 
-
-  puts("-----");
   printList(list);
 
   // hehe, tinha esquecido
   free(list->head);
-  free(list->end);
   free(list);
 
   return 0;
