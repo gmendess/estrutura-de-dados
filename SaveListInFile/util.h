4fcc489bc56229cdf948df4e176d64264dfb36db
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-07-22 22:47:18 -0300

    Finalmente o inicio dos testes de salvar uma lista encadeada em um arquivo binario

--- SaveListInFile/util.h
+++ SaveListInFile/util.h
@@ -1,11 +1,20 @@
 #ifndef MY_UTIL_FUNCTIONS_H
 #define MY_UTIL_FUNCTIONS_H
 
+#include "linked_list.h"
+
 /* 
   Funções pra manipulação do arquivo binário
 */
 
+#define OPEN_FAIL 10
+#define NULL_FILE 11
 
+// abre (se não existe, é criado) um arquivo binário no mode ab+ e retorna ponteiro para FILE
+FILE* openFile();
+
+// exporta nós de uma lista encadeada para um arquivo binário
+int exportList(FILE* file, const List* const list);
 
 
 #endif
