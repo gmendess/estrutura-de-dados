ca10f607e9314a2e23a063e1421c31b308350dd1
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-07-27 22:21:08 -0300

    Adicionando alguns comentarios e codigo de erro INVALID_FILE

--- SaveListInFile/util.h
+++ SaveListInFile/util.h
@@ -7,14 +7,19 @@
   Funções pra manipulação do arquivo binário
 */
 
-#define OPEN_FAIL 10
-#define NULL_FILE 11
+#define OPEN_FAIL    10
+#define NULL_FILE    11
+#define INVALID_FILE 12
 
-// abre (se não existe, é criado) um arquivo binário no mode ab+ e retorna ponteiro para FILE
+// abre (se não existe, é criado) um arquivo binário no mode rb e retorna ponteiro para FILE
 FILE* openFile();
 
 // exporta nós de uma lista encadeada para um arquivo binário
+// Nota: ele reabre o arquivo em modo wb para apagar o conteúdo antes de exportar os nós
 int exportList(FILE* file, const List* const list);
+
+// importa nós de uma lista encadeada para um arquivo binário
+int importList(FILE* file, List* const list);
 
 
 #endif
