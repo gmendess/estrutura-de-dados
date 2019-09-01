8766ef8410835204688fdd5c06328ba833be064b
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-09-01 15:07:18 -0300

    Enviando main.c

---
+++ lms/simpletron-vm/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "simpletron.h"
+
+int main() {
+
+  Simpletron* simpletron = createSimpletron();
+  loadInstructions(simpletron);
+  run(simpletron);
+  memoryDump(simpletron);
+
+  free(simpletron);
+
+
+  return EXIT_SUCCESS;
+}
