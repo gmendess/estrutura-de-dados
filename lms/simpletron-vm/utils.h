878e3ea046e49deefde8883261b6618073b10312
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-09-01 00:54:04 -0300

    Corrigindo grafia

--- lms/simpletron-vm/utils.h
+++ lms/simpletron-vm/utils.h
@@ -10,8 +10,8 @@ typedef long int ssize_t;
 #define SIMPLETRON_MEMORY 1000 // memória máxima do simpletron
 #define MAX_FGETS_INPUT 10 // quantidade máxima de bytes lidos por FGETS
 
-#define operand_module 100 // número usado no resto de divisão para achar o operando de uma instrucao. Ex.: 1234 % operand_module = 34
-#define operation_module 100 // número usado no resto de divisão para achar o operador de uma instrucao
+#define operand_module 100 // número usado no resto de divisão para achar o operando de uma instrução. Ex.: 1234 % operand_module = 34
+#define operation_module 100 // número usado no resto de divisão para achar a operação de uma instrucao
 
 // macros de erro
 #define NULL_POINTER_ERROR 10
