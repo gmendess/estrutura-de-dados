29076077149530aa6b4bd2aa1034eb97d84ff3ed
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-09-02 23:27:09 -0300

    Testes da tabela de simbolo e do vetor de instruçoes

--- lms/lms-compiler/main.c
+++ lms/lms-compiler/main.c
@@ -9,6 +9,13 @@ int main() {
   Table* table = createTable();
 
   readInstructions(lms, table, file);
+  //for(int x = 0; x < table->size; x++)
+  //  if(table->vector[x].type != 'L')
+  //    printf("%d\n", table->vector[x].locale);
+
+  for(int x = 0; x < SIMPLETRON_MEMORY; x++)
+    if(lms->instructions[x] != 0)
+      printf("%ld\n", lms->instructions[x]);
 
   return EXIT_SUCCESS;
 }
