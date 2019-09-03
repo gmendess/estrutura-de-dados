7faddcc6e7ef127839d37ba70ede129ddfa6ba1d
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-09-02 23:26:37 -0300

    Prototipos de funçao e algumas funçoes-macro para abstrair algumas verificaçoes, como comparaçao de strings para identificar a instruçao a ser montada

--- lms/lms-compiler/lmsc.h
+++ lms/lms-compiler/lmsc.h
@@ -1,6 +1,7 @@
 #ifndef _LMSC_H
 #define _LMSC_H
 
+#include <string.h>
 #include "utils.h"
 
 // Struct que representa casa símbolo na tabela de símbolos
@@ -21,7 +22,8 @@ typedef struct _symbol_table Table;
 // Núcleo do compilador. Contém tanto o vetor de flags, quanto as instruções a serem executadas pelo Simpletron
 struct _lms {
   size_t instruction_count; // contador para instruções (deve ser inicializado com 0)
-  size_t variable_count; // contador para variáveis/constantes (deve ser inicializado com SIMPLETRON_MEMORY - 1)
+  size_t variable_pos; // contador de posições para variáveis/constantes (deve ser inicializado com SIMPLETRON_MEMORY - 1)
+  size_t curr_variable;
   size_t instructions[SIMPLETRON_MEMORY]; // instruções a serem executadas em lms
   int flags[SIMPLETRON_MEMORY]; // marcações para referências antecipadas
 };
@@ -34,5 +36,12 @@ Table* createTable();
 
 void readInstructions(LMS* lms, Table* table, FILE* const file);
 void addSymbol(Table* table, int locale, int value, char type);
+int symbolExists(Table* table, int value);
+
+#define getNextToken() (strtok(NULL, " "))
+
+#define isInput(token)  (strcmp(token, "input") == 0)
+#define isPrint(token) (strcmp(token, "print") == 0)
+#define isEnd(token)   (strcmp(token, "end") == 0)
 
 #endif // _LMSC_H
