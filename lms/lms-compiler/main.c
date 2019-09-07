82601def61d2a3920e26a294c1b55030b3c706a2
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-09-07 17:07:45 -0300

    Alguns testes para ver a saida da tabela de simbolos

--- lms/lms-compiler/main.c
+++ lms/lms-compiler/main.c
@@ -9,10 +9,13 @@ int main() {
   Table* table = createTable();
 
   readInstructions(lms, table, file);
-  //for(int x = 0; x < table->size; x++)
-  //  if(table->vector[x].type != 'L')
-  //    printf("%d\n", table->vector[x].locale);
 
+  puts("\nTabela de símbolos:");
+  for(int x = 0; x < table->size; x++)
+    if(table->vector[x].type != 'L')
+      printf("%d %d\n", table->vector[x].locale, table->vector[x].value);
+
+  puts("\nInstruções em LMS:");
   for(int x = 0; x < SIMPLETRON_MEMORY; x++)
     if(lms->instructions[x] != 0)
       printf("%ld\n", lms->instructions[x]);
