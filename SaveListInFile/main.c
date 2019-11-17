e3f42fede9ffdeb95f67fe5558fbbd85ef479bd5
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-11-17 19:18:10 -0300

    Adicionado exemplo de importaçao e de exportaçao

--- SaveListInFile/main.c
+++ SaveListInFile/main.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-#include "util.c"
-#include "linked_list.c"
+#include "util.h"
+#include "linked_list.h"
+
+/*
+  Compile com:
+  gcc main.c util.c linked_list.c -o main
+*/
 
 int main() {
 
@@ -12,10 +17,27 @@ int main() {
   FILE* file = openFile();
   assert(file != NULL);
 
-  importList(file, list);
+  /* TESTE PARA EXPORTAR LISTA
+  push(list, "Primeiro nó da lista!");
+  push(list, "Mensagem qualquer :)");
+  push(list, "AAAA BBBB CCCC");
   printList(list);
+  
+  if(exportList(file, list) == SUCCESS)
+    puts("Lista exportada com sucesso para o arquivo!");
+  else
+    puts("Falha ao exportar lista!");
+  */
 
-  printf("%X\n", 0b1010);
+  /* TESTE PARA IMPORTAR LISTA
+  printList(list); // primeiro print, lista vazia
+  if(importList(file, list) == SUCCESS) {
+    puts("Dados importados com sucesso para a lista!");
+    printList(list);
+  }
+  else
+    puts("Falha ao importar dados para a lista!");
+  */
 
   deleteList(&list);
   fclose(file);
