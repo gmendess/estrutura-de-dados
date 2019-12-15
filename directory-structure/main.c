498c4e33c1ec35ce43ddcfc1e1b7be68bcf882a2
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-12-15 17:43:03 -0300

    Teste geral

--- directory-structure/main.c
+++ directory-structure/main.c
@@ -1,15 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+#include <string.h>
 #include "directory_structure.h"
 
 int main(int argc, char* argv[]) {
 
   Directory* new_directory = create_directory("teste");
-  puts(new_directory->name);
+  assert(new_directory != NULL);
+  assert(new_directory->name != NULL);
+  assert(strcmp(new_directory->name, "teste") == 0);
+  assert(new_directory->files == NULL);
+  assert(new_directory->sub_dirs == NULL);
 
   File* new_file = create_file("arquivo.txt", 12);
-  puts(new_file->name);
-  printf("%lu\n", new_file->size);
+  assert(new_file != NULL);
+  assert(new_file->name != NULL);
+  assert(strcmp(new_file->name, "arquivo.txt") == 0);
+  assert(new_file->size == 12);
+  assert(new_file->next == NULL);
 
   return EXIT_SUCCESS;
 }
