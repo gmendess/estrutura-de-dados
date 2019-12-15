6192dbbb5303a1ed308b1b46f1429327ebf52784
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-12-15 17:03:46 -0300

    Construindo cabeçalho da struct directory e file

--- directory-structure/directory_structure.h
+++ directory-structure/directory_structure.h
@@ -1,4 +1,17 @@
 #ifndef _DIRECTORY_STRUCTURE_H
 #define _DIRECTORY_STRUCTURE_H
 
+struct directory {
+  char* name;
+  struct directory* sub_dirs; // diretórios filhos
+  struct directory* next; // diretórios irmãos
+  struct file* files; // arquivos presentes no diretório
+};
+
+struct file {
+  char* name;
+  size_t size; // tamanho do arquivo
+  struct file* next; // arquivos irmãos
+};
+
 #endif // _DIRECTORY_STRUCTURE_H
