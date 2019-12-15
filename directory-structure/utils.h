519ce5213e88728c5884d4f8de4c7d4ba61cca34
Gabriel Mendes <gabriel.mendespng@gmail.com>
2019-12-15 17:27:12 -0300

    Criando macro para verificar alocação de memória

--- directory-structure/utils.h
+++ directory-structure/utils.h
@@ -1,6 +1,16 @@
 #ifndef _DIRECTORY_STRUCTURE_UTILS_H
 #define _DIRECTORY_STRUCTURE_UTILS_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "directory_structure.h"
+
+#define CHECK_IF_NULL(pointer) do {                     \
+  if(pointer == NULL) {                                 \
+    fprintf(stderr, "Malloc fail at %s()\n", __func__); \
+    exit(1);                                            \
+  }                                                     \
+} while(0)
+
 
 #endif // _DIRECTORY_STRUCTURE_UTILS_H
